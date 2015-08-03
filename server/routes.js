3fa510fb313ad87675765c45c0f180a8ff8166e4
Una Gauper <umgauper@gmail.com>
2015-08-03 14:56:27 -0700

    added npm module 'request'

--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,8 @@ var errors = require('./components/errors');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/books', require('./api/book'));
+  app.use('/api/search', require('./api/search'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
