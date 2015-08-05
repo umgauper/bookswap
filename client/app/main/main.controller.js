5ddd67183ce66c2d659c71ad1670ed383b59cd35
Una Gauper <umgauper@gmail.com>
2015-08-04 19:39:28 -0700

    removed alert, changed smallThumbnail to .thumbnail

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,14 +3,25 @@
   angular.module('bookswapApp')
     .controller('MainCtrl', function ($scope, $http, Auth, User) {
 
-    $scope.getBooks = function(_id) {
+
+    $scope.page = 'myBooks';
+
+
+    $scope.getAllBooks = function() {
+      $http.get('api/books').success(function(data) {
+        $scope.allBooks = data;
+        console.log(data);
+      });
+    };
+
+    $scope.getMyBooks = function(_id) {
         $http.get('api/books/' + _id).success(function(data) {
                 $scope.myBooks = data;
         });
       };
 
-    $http.get('api/users/me').success(function(data) { //User.get() and Auth.getCurrentUser()._id were returning undefined, so I used direct get request to user api instead
-      $scope.getBooks(data._id);
+    $http.get('api/users/me').success(function(data) { //User.get() and Auth.getCurrentUser()._id were returning undefined, so I used a direct get request to user api instead
+      $scope.getMyBooks(data._id);
     });
 
 
@@ -22,10 +33,6 @@
       return Auth.getCurrentUser()._id
     };
 
-      alert($scope.userIDKnown());
-
-
-
     $scope.addBook = function() {
       $http.get('api/search/' + $scope.newBook).success(function(data) {
 
@@ -34,18 +41,26 @@
         var bookObj = JSON.parse(data.body);
         var volumeInfo = bookObj.items[0].volumeInfo;
 
+        console.log(volumeInfo);
+
         var name = volumeInfo.title;
-        var imageURL = volumeInfo.imageLinks.smallThumbnail;
+
+        if(volumeInfo.imageLinks) {
+          var imageURL = volumeInfo.imageLinks.thumbnail; //If the title doesn't have a thumbnail image, assign generic book cover
+        } else {
+          var imageURL = 'http://i.istockimg.com/file_thumbview_approve/5523935/3/stock-photo-5523935-antique-leather-book-cover.jpg';
+        }
+
         var isbn = volumeInfo.industryIdentifiers[0].identifier;
 
         $http.post('api/books', {owner: owner,
                                   name: name,
                                   isbn: isbn,
                                   imageUrl: imageURL
-        }).success(function(data) {
-            $scope.getBooks(owner);
+        })
+        .success(function(data) {
+            $scope.getMyBooks(owner);
         });
-
       });
     };
   });
