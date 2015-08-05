d4f142de33444dd16472a800cde4e15ce1061c4f
Una Gauper <umgauper@gmail.com>
2015-08-04 19:41:57 -0700

    refactored code so get request to api/books returns list of all books

--- server/api/book/index.js
+++ server/api/book/index.js
@@ -5,7 +5,8 @@ var controller = require('./book.controller');
 
 var router = express.Router();
 
-router.get('/:owner', controller.index);
+router.get('/:owner', controller.indexByOnwer);
+router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
