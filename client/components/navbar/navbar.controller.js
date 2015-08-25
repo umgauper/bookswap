cf50fbdb536f670b8330bb102cbb2633dc527115
Una Gauper <umgauper@gmail.com>
2015-08-25 09:46:16 -0700

    fixed register button

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -14,7 +14,7 @@ angular.module('bookswapApp')
 
     $scope.logout = function() {
       Auth.logout();
-      $location.path('/login');
+      $location.path('/');
     };
 
     $scope.isActive = function(route) {
